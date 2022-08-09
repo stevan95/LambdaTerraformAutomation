b8cf3997904b9b45af0b0714bcad03fcd2b2d86e
Stevan <mstevan95@gmail.com>
2022-08-09 13:41:55 -0700

    Added variable which contain list of policies.

--- var.tf
+++ var.tf
@@ -7,3 +7,10 @@ variable "s3_bucket_names" {
   type = list
   default = ["serverless-video-upload-test01-atlas", "serverless-video-transcoded-test01-atlas"]
 }
+
+variable "iam_policy_arn" {
+  description = "IAM Policy to be attached to role"
+  type = list
+  default = ["arn:aws:iam::aws:policy/AWSLambdaExecute", "arn:aws:iam::aws:policy/AmazonElasticTranscoder_JobsSubmitter"]
+}
+
